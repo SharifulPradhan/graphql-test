35ad027653573d1c705925ba0166a325cba0bad0
Shariful Pradhan Hridoy <sharifulpradhanhridoy@gmail.com>
2022-04-13 17:29:36 +0600

    initial setup of the server and graphql

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import schema from './schema';
+import { graphqlHTTP } from 'express-graphql';
+
+const app = express();
+
+app.get('/', (req, res)=> {
+  res.send('Graphql is amazing:');
+});
+
+const root = {vaccine: () => true};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(8080, () => console.log('Running on server port localhost:8080/graphql'));
